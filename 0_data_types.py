a888578cdcd7b64895eaf6946d3ed19a217b0efd
rdkkz <Bolszewo11>
2021-08-07 10:02:46 +0200

    Initial commit

---
+++ 0_data_types.py
@@ -0,0 +1,11 @@
+"""There are numerous datatypes in python, those included and those that need import.
+Here we will discuss built-in datatypes in Python
+"""
+
+"""Strings, we all like them. It is always nice to see good content in a pair of nice quotes :D"""
+string_1: str = "Hello world"
+string_2: str = "Some random message"
+print(f"{string_1 = }, some separator message, {string_2 = }\n")
+
+"""Integers, numbers without floating points"""
+
