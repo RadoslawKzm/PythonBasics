935fe2aa94f7cdabf5c85d39c590065d4c0ad708
rdkkz <Bolszewo11>
2021-08-22 15:56:46 +0200

    some chaotic file operations example

---
+++ 8_file_operations.py
@@ -0,0 +1,42 @@
+# 1,dupa,test
+# 2,nazwa,nazwisko
+# 3,andrzej,malczewski
+
+
+file = open("0_data_types.py", "r")
+
+txt = file.readline()
+
+
+_ = file.__next__()
+_ = file.__next__()
+_ = file.__next__()
+_ = file.__next__()
+_ = file.__next__()
+
+tst = file.__iter__()
+file.close()
+
+with open("0_data_types.py", "r") as f:
+    line1 = f.readline()
+    line2 = f.readline()
+    line3 = f.readline()
+    line4 = f.readline()
+
+
+class FileWrapper:
+    def __enter__(self):
+        return "DUPA"
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        if any((exc_type, exc_val, exc_tb)):
+            print("Error present ")
+            """False when you want to propagate exception stopping the program, True if you want to suppress"""
+            return True
+        print("doing something no matter if exception occured")
+
+
+context_manager = FileWrapper()
+with context_manager as var:
+    print("inside context")
+    raise KeyError("simulating error")
