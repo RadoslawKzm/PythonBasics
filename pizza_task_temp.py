3d0addd4a159118f984fd19da349f8fe0c129af0
rdkkz <Bolszewo11>
2021-08-22 12:40:31 +0200

    added businnes requirements

---
+++ pizza_task_temp.py
@@ -0,0 +1,10 @@
+"""We want to creat pizzas
+input: name_of_pizza, diameter, toppings, price, costs
+functionalities:
+- create pizza object with given parameters.
+- compare pizzas which one is more area for value cm2/zl
+- calculate overall gross income
+- calculate overall costs
+- calculate overall net income
+"""
+
